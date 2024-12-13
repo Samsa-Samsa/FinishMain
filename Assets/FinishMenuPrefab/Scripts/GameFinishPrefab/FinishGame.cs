f9ebf497303e4be16b6598bf6f8206162c734fc2
Samsa-Samsa <gr.samsaa@gmail.com>
2024-12-13 07:01:26 -0800

    Com

---
+++ Assets/FinishMenuPrefab/Scripts/GameFinishPrefab/FinishGame.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FinishMenuPrefab.Scripts.GameFinishPrefab
+{
+    // Class that handles game finishing logic
+    public abstract class FinishGame 
+    {
+        // Event to notify when the game is finished
+        public static  Action OnGameFinish;
+    }
+}
---
+++ Assets/FinishMenuPrefab/Scripts/GameFinishPrefab/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace FinishMenuPrefab.Scripts.GameFinishPrefab
+{
+    public class TestClass : MonoBehaviour
+    {
+        [SerializeField] private int myInt;
+        private void Update()
+        {
+            if (myInt != 1) return;
+            FinishGame.OnGameFinish.Invoke();
+            myInt = 2;
+            
+        }
+    }
+}
